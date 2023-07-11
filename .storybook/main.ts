573dfc7c40faeaab48a0aa7b17b4b5669bed0d4c
solomon mulatu <mulatusolomon@gmail.com>
2023-07-11 11:00:54 +0300

    build: implement storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,32 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-css-modules-preset',
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: 'storybook-addon-sass-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {
+      builder: '@storybook/builder-webpack5',
+    },
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
